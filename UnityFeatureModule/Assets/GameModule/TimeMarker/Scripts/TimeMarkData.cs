7c1495c073ec4370b6921aaacb61c3dd2da2b12d
huylqnothing <huylqnothing@gmail.com>
2024-11-05 14:52:54 +0700

    Timemark modified

--- UnityFeatureModule/Assets/GameModule/TimeMarker/Scripts/TimeMarkData.cs
+++ UnityFeatureModule/Assets/GameModule/TimeMarker/Scripts/TimeMarkData.cs
@@ -30,20 +30,34 @@
             data.TimeMarks[key] = time;
         }
         
+        /// <summary>
+        /// Retrieves the time mark associated with the specified key. 
+        /// If the key does not exist, creates a new time mark with the current time, 
+        /// stores it in the TimeMarks dictionary, and returns false.
+        /// </summary>
+        /// <param name="key">The unique key identifying the time mark.</param>
+        /// <param name="time">The output parameter that will hold the retrieved or newly created time mark.</param>
+        /// <returns>
+        /// True if the time mark was found and retrieved; false if a new time mark was created and stored.
+        /// </returns>
         public bool GetTimeMark(string key, out DateTime time)
         {
-            var data = (TimeMarkData) this.localData;
-            if(data.TimeMarks.ContainsKey(key))
+            var data = (TimeMarkData)this.localData;
+
+            if (data.TimeMarks.ContainsKey(key))
             {
                 time = data.TimeMarks[key];
                 return true;
             }
             else
             {
-                time = DateTime.Now;
+                // Create a new time mark, store it, and return false
+                time                = DateTime.Now;
+                data.TimeMarks[key] = time; // Store the new time mark for future use
                 return false;
             }
         }
+
         
         public void RemoveTimeMark(string key)
         {
