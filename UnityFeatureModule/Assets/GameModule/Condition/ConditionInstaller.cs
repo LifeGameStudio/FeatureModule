19290f0ebfddf7964b9ec709d3c6013adb2bc385
huylqnothing <huylqnothing@gmail.com>
2024-11-19 11:15:31 +0700

    Update ConditionInstaller

---
+++ UnityFeatureModule/Assets/GameModule/Condition/ConditionInstaller.cs
@@ -0,0 +1,22 @@
+﻿namespace GameModule.Condition
+{
+    using Cysharp.Threading.Tasks;
+    using FeatureTemplate.Scripts.Services;
+    using GameFoundation.Scripts.Utilities.Extension;
+    using Zenject;
+
+    public class ConditionInstaller : Installer<ConditionInstaller>
+    {
+        public override void InstallBindings()
+        {
+            this.Container.BindInterfacesAndSelfTo<ConditionHandler>().AsCached().NonLazy();
+            this.OnAfterDataLoaded();
+        }
+        
+        private async void OnAfterDataLoaded()
+        {
+            await UniTask.WaitUntil(() => this.Container.Resolve<FeatureDataState>().IsBlueprintAndLocalDataLoaded);
+            this.Container.BindInterfacesAndSelfToAllTypeDriveFrom<ICondition>();
+        }
+    }
+}
