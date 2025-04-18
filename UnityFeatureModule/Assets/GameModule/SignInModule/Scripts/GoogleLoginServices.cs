3721f9db8fa948f8850d0b84169da26c74851fbd
hoanghai145 <hoangthanhhai1308@gmail.com>
2025-04-18 10:21:31 +0700

    fix bug

--- UnityFeatureModule/Assets/GameModule/SignInModule/Scripts/GoogleLoginServices.cs
+++ UnityFeatureModule/Assets/GameModule/SignInModule/Scripts/GoogleLoginServices.cs
@@ -1,29 +1,48 @@
 ﻿namespace GameModule.SignInModule.Scripts
 {
+    using System;
+    using System.Threading;
     using Assets.SimpleGoogleSignIn.Scripts;
     using Cysharp.Threading.Tasks;
 
     public class GoogleLoginServices : ILoginServices
     {
+        private CancellationTokenSource signInCts;
+
         public async UniTask<(string, string)> SignIn()
         {
+            this.signInCts?.Cancel();
+            this.signInCts = new CancellationTokenSource();
+
             var result     = ("", "");
             var isComplete = false;
 
+            var token = this.signInCts.Token;
+
             GoogleAuth.GetAccessToken((b, s, arg3) =>
             {
+                if (token.IsCancellationRequested) return;
+
                 if (b)
                 {
                     result = (arg3.IdToken, arg3.RefreshToken);
                 }
-                isComplete = true;
 
+                isComplete = true;
             });
 
-            await UniTask.WaitUntil(() => isComplete);
+            try
+            {
+                await UniTask.WaitUntil(() => isComplete, cancellationToken: token);
+            }
+            catch (OperationCanceledException)
+            {
+                return ("", "");
+            }
 
             return result;
         }
+
 
         public void ClearSession() { SavedAuth.Instance?.Delete(); }
     }
